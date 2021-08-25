2925fa066b86be347401db92f10383e6b7eae3dd
priya-jalani <72224049+priya-jalani@users.noreply.github.com>
2021-08-25 17:41:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var myengine, myworld
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400,400);
+  myengine = Engine.create()
+  myworld = myengine.world;
+
+  var options = {
+    isStatic : true
+  }
+
+  object = Bodies.rectangle(200, 380, 400, 20, options)
+  World.add(myworld, object)
+
+  console.log(object)
+  
+
+  
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(0); 
+  Engine.update(myengine)
+  rectMode (CENTER)
+  rect(object.position.x, object.position.y, 400, 20)
+  //console.log(object.position.x)
+  //console.log(object.position.y)
+  
 }
